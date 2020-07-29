56acbf9716d6dfd88fcc5ff00b02dd3baae78465
maiktiltado <bokunoherodiscord123@gmail.com>
2020-07-29 08:31:37 -0300

    fds

--- App/dbConnection/index.js
+++ App/dbConnection/index.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://heroku_csbtxjp1:pf9uunot442i6fp2tjmqtrielk@ds339648.mlab.com:39648/heroku_csbtxjp1", {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = mongoose
